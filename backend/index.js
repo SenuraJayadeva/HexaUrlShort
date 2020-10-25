67e49e15943644e4d5cce2431f13fc0baeac386b
SenuraJayadeva <senurajayadeva@gmail.com>
2020-10-25 15:31:20 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+
+require("dotenv").config();
+
+const app = express();
+
+//connectdb
+connectDB();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//define routes
+app.use("/", require("./routes/index"));
+app.use("/api/url", require("./routes/url"));
+
+app.listen(port, () => {
+  console.log("server started on " + port);
+});
---
+++ backend/routes/url.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const validUrl = require("valid-url");
+const shortid = require("shortid");
+const config = require("config");
+
+const Url = require("../models/Url");
+
+//@route POST /api/url/shorten
+//@desc Create short url
+router.post("/shorten", async (req, res) => {
+  const { longUrl } = req.body;
+
+  const baseUrl = config.get("baseUrl");
+
+  //check base url
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(401).json("Invalid base url");
+  }
+
+  //Create an url code
+  const urlCode = shortid.generate();
+
+  //check the long url
+  if (validUrl.isUri(longUrl)) {
+    try {
+      let url = await Url.findOne({ longUrl });
+
+      if (url) {
+        res.json(url);
+      } else {
+        const shortUrl = baseUrl + "/" + urlCode;
+
+        url = new Url({
+          longUrl,
+          shortUrl,
+          urlCode,
+          date: new Date(),
+        });
+
+        await url.save().then(() => {
+          res.json(url);
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.status(401).json("Invalid long url");
+  }
+});
+
+module.exports = router;
